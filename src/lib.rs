11e41004cdd74c0c708e244d20f133b585f0313c
s.kazemi <sinasharifkazemi@proton.me>
2025-02-13 16:08:39 +0330

    median

--- src/lib.rs
+++ src/lib.rs
@@ -10,8 +10,28 @@ fn mean(numbers: Vec<f64>) -> f64 {
     sum / (numbers.len() as f64)
 }
 
+#[pyfunction]
+fn median(mut numbers: Vec<f64>) -> f64 {
+    if numbers.is_empty() {
+        return 0.0;
+    }
+
+    numbers.sort_by(|a, b| a.partial_cmp(b).unwrap());
+
+    let n = numbers.len();
+
+    if n % 2 == 0 {
+        let mid1 = numbers[(n / 2) - 1];
+        let mid2 = numbers[n / 2];
+        (mid1 + mid2) / 2.0
+    } else {
+        numbers[n / 2]
+    }
+}
+
 #[pymodule]
 fn ractors(m: &Bound<'_, PyModule>) -> PyResult<()> {
     m.add_function(wrap_pyfunction!(mean, m)?)?;
+    m.add_function(wrap_pyfunction!(median, m)?)?;
     Ok(())
 }
