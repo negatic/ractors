ac4bba47246b3bb3a3d04ad6075437c24a542b33
s.kazemi <sinasharifkazemi@proton.me>
2025-02-13 16:39:23 +0330

    std

--- src/lib.rs
+++ src/lib.rs
@@ -29,9 +29,24 @@ fn median(mut numbers: Vec<f64>) -> f64 {
     }
 }
 
+#[pyfunction]
+fn std_dev(numbers: Vec<f64>) -> f64 {
+    if numbers.is_empty() {
+        return 0.0;
+    }
+
+    let mean = numbers.iter().sum::<f64>() / numbers.len() as f64;
+
+    let variance = numbers.iter().map(|x| (x - mean).powi(2)).sum::<f64>() / numbers.len() as f64;
+
+    variance.sqrt()
+}
+
+
 #[pymodule]
 fn ractors(m: &Bound<'_, PyModule>) -> PyResult<()> {
     m.add_function(wrap_pyfunction!(mean, m)?)?;
     m.add_function(wrap_pyfunction!(median, m)?)?;
+    m.add_function(wrap_pyfunction!(std_dev, m)?)?;
     Ok(())
 }
